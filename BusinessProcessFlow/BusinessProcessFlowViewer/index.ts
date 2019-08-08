e1fc0fbb6f2970b5bda4a946dfb39800b761bb39
Allan De Castro <allan.de.castro@avanade.com>
2019-08-08 18:06:04 +0200

    V1.0.0.0

--- BusinessProcessFlow/BusinessProcessFlowViewer/index.ts
+++ BusinessProcessFlow/BusinessProcessFlowViewer/index.ts
@@ -15,6 +15,7 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 	private _notActiveTextColor: string;
 	private _progressTrackLineColor: string;
 	private _pulseColor: string;
+	private _viewId: string;
 	constructor() {
 
 	}
@@ -30,67 +31,14 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 		// Add control initialization code
 		this._context = context;
 		this._container = container;
-		//this._context.mode.trackContainerResize(true);
-
-		this._container = document.createElement("div");
 		//Get all parameters.
-		this._parametersBPF = this._context.parameters.parametersBPF == undefined ? "" : this._context.parameters.parametersBPF.raw;
-		this._completedColor = this._context.parameters.completedColor == undefined ? "" : this._context.parameters.completedColor.raw;
-		this._completedTextColor = this._context.parameters.completedTextColor == undefined ? "" : this._context.parameters.completedTextColor.raw;
-		this._activeColor = this._context.parameters.activeColor == undefined ? "" : this._context.parameters.activeColor.raw;
-		this._activeTextColor = this._context.parameters.activeTextColor == undefined ? "" : this._context.parameters.activeTextColor.raw;
-		this._notActiveColor = this._context.parameters.notActiveColor == undefined ? "" : this._context.parameters.notActiveColor.raw;
-		this._notActiveTextColor = this._context.parameters.notActiveTextColor == undefined ? "" : this._context.parameters.notActiveTextColor.raw;
-		this._progressTrackLineColor = this._context.parameters.progressTrackLineColor == undefined ? "" : this._context.parameters.progressTrackLineColor.raw;
-		this._pulseColor = this._context.parameters.pulseColor == undefined ? "" : this._context.parameters.pulseColor.raw;
-		if (this._parametersBPF.length == 0)
-			console.log("[BPFV][INIT] Parameters BPF is empty...");
+		this.getAllParameters();
+		this.checkBPFParameterIsValid();
 		// Adding the main table to the container DIV.
+		this._container = document.createElement("div");
 		container.appendChild(this._container);
-
 		//try to create style for color variable
-		var styleNode = document.createElement('style');
-		styleNode.type = "text/css";
-		//We create style css here because we use some parameters.
-		if (this._progressTrackLineColor.length > 0) {
-			var styleText = document.createTextNode('.progress-track { background-color: ' + this._progressTrackLineColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._notActiveColor.length > 0) {
-			var styleText = document.createTextNode('.progress .progress-step:before { border: 4px solid ' + this._notActiveColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._notActiveTextColor.length > 0) {
-			var styleText = document.createTextNode('.is-notactive { color: ' + this._notActiveTextColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		//    animation: pulse 2s infinite;
-		if (this._activeColor.length > 0) {
-			if (this._pulseColor && this._pulseColor.length > 0) 
-				var styleText = document.createTextNode('.progress .progress-step.is-active:before { border: 4px solid ' + this._activeColor + '; animation: pulse 2s infinite; }');
-			else
-				var styleText = document.createTextNode('.progress .progress-step.is-active:before { border: 4px solid ' + this._activeColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._activeTextColor.length > 0) {
-			var styleText = document.createTextNode('.is-active { color: ' + this._activeTextColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._completedTextColor.length > 0) {
-			var styleText = document.createTextNode('.is-complete { color: ' + this._completedTextColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._completedColor.length > 0) {
-			var styleText = document.createTextNode('.progress .progress-step.is-complete:before { background: ' + this._completedColor + '; } ');
-			styleNode.appendChild(styleText);
-			var styleText = document.createTextNode('.progress .progress-step.is-complete:after { background: ' + this._completedColor + '; } ');
-			styleNode.appendChild(styleText);
-		}
-		if (this._pulseColor && this._pulseColor.length > 0) {
-			var styleText = document.createTextNode("@keyframes pulse {0% {box-shadow: 0 0 0 0 " + this.hexToRgba(this._pulseColor, 40) + " ;} 70% {box-shadow: 0 0 0 10px " + this.hexToRgba(this._pulseColor, 100) + ";} 100% {box-shadow: 0 0 0 0 " + this.hexToRgba(this._pulseColor, 100) + ";} }} ");
-			styleNode.appendChild(styleText);
-		}
-		document.getElementsByTagName('head')[0].appendChild(styleNode);
+		this.initializeCustomCSSWithParameters();
 	}
 	/**
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
@@ -102,9 +50,7 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 		//We make sure that the data set loading is finished.
 		if (!context.parameters.dataSet.loading) {
 			//We remove all items to make sure that if you perform a refresh, it will not add the records again.
-			while (this._container.firstChild) {
-				this._container.removeChild(this._container.firstChild);
-			}
+			this.removeChildItems();
 			//Parse the Parameters and make sure it contains datas.
 			let jsonParametersBPF = JSON.parse(this._parametersBPF);
 			if (!jsonParametersBPF || jsonParametersBPF.bpfs == null && jsonParametersBPF.bpfs.length == 0) {
@@ -112,6 +58,7 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 				return;
 			}
 			//Start the process for each records in the dataset (subgrid,view...).
+
 			let thisFunction = this;
 			let targetEntityLogicalName: string = context.parameters.dataSet.getTargetEntityType();
 			for (let currentRecordId of context.parameters.dataSet.sortedRecordIds) {
@@ -132,10 +79,23 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 							//Building the DOM element...
 							let progresDiv: HTMLDivElement = document.createElement("div");
 							progresDiv.classList.add("progress");
+							progresDiv.classList.add("progress" + thisFunction._viewId);
 							progresDiv.setAttribute(targetEntityLogicalName, currentRecordId.toUpperCase());
 							let progresTrackDiv: HTMLDivElement = document.createElement("div");
 							progresTrackDiv.classList.add("progress-track");
-							progresDiv.appendChild(progresTrackDiv);
+							progresTrackDiv.classList.add("progress-track" + thisFunction._viewId);
+
+							progresDiv.appendChild(progresTrackDiv)
+							progresDiv.onclick = function () {
+								let entityReference = context.parameters.dataSet.records[currentRecordId].getNamedReference();
+								let entityFormOptions = {
+									entityName: targetEntityLogicalName,
+									entityId: entityReference.id,
+									openInNewWindow: true
+								}
+								console.log("OnCLik :  logical name = " + targetEntityLogicalName + " id = " + currentRecordId);
+								context.navigation.openForm(entityFormOptions);
+							};
 							foundBPF = true; // We set this var to true to stop the foreach loop for this record.
 							//Get process Unique Identifier and Active Stage Unique identifier for this BPF instance.
 							let activeStageIdCurrentRecord: string = stageId[0];
@@ -150,19 +110,27 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 							JSON.parse(jsonObjectStageId, (key, value) => {
 								if (typeof value === 'string') {
 									let progresStageDiv: HTMLDivElement = document.createElement("div");
+									//+this._viewId
 									progresStageDiv.classList.add("progress-step");
+									progresStageDiv.classList.add("progress-step" + thisFunction._viewId);
 									progresStageDiv.classList.add("is-notactive");
+									progresStageDiv.classList.add("is-notactive" + thisFunction._viewId);
 									//Test if this is the Active Stage.
 									if (value === activeStageIdCurrentRecord) {
 										activeStageFound = true;
 										console.log("\n [BPFV] Active stage found for " + currentRecordId + " : " + thisFunction.toPascalCase(stageProcess.stage.name[Number(key)]));//cast key an number
 										progresStageDiv.classList.add("is-active");
+										progresStageDiv.classList.add("is-active" + thisFunction._viewId);
 										progresStageDiv.classList.remove("is-notactive");
+										progresStageDiv.classList.remove("is-notactive" + thisFunction._viewId);
+
 									}
 									//If we haven't find the active stage that means previous stage is completed...or not yet active
 									if (!activeStageFound) {
 										progresStageDiv.classList.add("is-complete");
+										progresStageDiv.classList.add("is-complete" + thisFunction._viewId);
 										progresStageDiv.classList.remove("is-notactive");
+										progresStageDiv.classList.remove("is-notactive" + thisFunction._viewId);
 									}
 									console.log("\n [BPFV] Stage : " + thisFunction.toPascalCase(stageProcess.stage.name[Number(key)]) + " / " + value.toUpperCase());
 									progresStageDiv.innerText = thisFunction.toPascalCase(stageProcess.stage.name[Number(key)]);
@@ -180,6 +148,74 @@ export class BusinessProcessFlowViewer implements ComponentFramework.StandardCon
 				});
 			}
 		}
+	}
+	private getAllParameters() {
+		this._parametersBPF = this._context.parameters.parametersBPF == undefined ? "" : this._context.parameters.parametersBPF.raw;
+		this._viewId = this._context.parameters.dataSet.getViewId() == undefined ? "" : this._context.parameters.dataSet.getViewId();
+		this._completedColor = this._context.parameters.completedColor == undefined ? "" : this._context.parameters.completedColor.raw;
+		this._completedTextColor = this._context.parameters.completedTextColor == undefined ? "" : this._context.parameters.completedTextColor.raw;
+		this._activeColor = this._context.parameters.activeColor == undefined ? "" : this._context.parameters.activeColor.raw;
+		this._activeTextColor = this._context.parameters.activeTextColor == undefined ? "" : this._context.parameters.activeTextColor.raw;
+		this._notActiveColor = this._context.parameters.notActiveColor == undefined ? "" : this._context.parameters.notActiveColor.raw;
+		this._notActiveTextColor = this._context.parameters.notActiveTextColor == undefined ? "" : this._context.parameters.notActiveTextColor.raw;
+		this._progressTrackLineColor = this._context.parameters.progressTrackLineColor == undefined ? "" : this._context.parameters.progressTrackLineColor.raw;
+		this._pulseColor = this._context.parameters.pulseColor == undefined ? "" : this._context.parameters.pulseColor.raw;
+
+	}
+	private removeChildItems() {
+		while (this._container.firstChild) {
+			this._container.removeChild(this._container.firstChild);
+		}
+	}
+	private checkBPFParameterIsValid() {
+		if (this._parametersBPF.length == 0) {
+			console.log("[BPFV][INIT] Parameters BPF is empty...");
+			this.destroy()
+		}
+	}
+	private initializeCustomCSSWithParameters() {
+		var styleNode = document.createElement('style');
+		styleNode.type = "text/css";
+		//We create style css here because we use some parameters.
+		if (this._progressTrackLineColor.length > 0) {
+			var styleText = document.createTextNode('.progress-track' + this._viewId + ' { background-color: ' + this._progressTrackLineColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._notActiveColor.length > 0) {
+			var styleText = document.createTextNode('.progress' + this._viewId + ' .progress-step:before { border: 4px solid ' + this._notActiveColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._notActiveTextColor.length > 0) {
+			var styleText = document.createTextNode('.is-notactive' + this._viewId + ' { color: ' + this._notActiveTextColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+
+		if (this._activeColor.length > 0) {
+			if (this._pulseColor && this._pulseColor.length > 0)
+				var styleText = document.createTextNode('.progress' + this._viewId + ' .progress-step.is-active:before { border: 4px solid ' + this._activeColor + '; animation: pulse' + this._viewId + ' 2s infinite; }');
+			else
+				var styleText = document.createTextNode('.progress' + this._viewId + ' .progress-step.is-active:before { border: 4px solid ' + this._activeColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._activeTextColor.length > 0) {
+			var styleText = document.createTextNode('.is-active' + this._viewId + ' { color: ' + this._activeTextColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._completedTextColor.length > 0) {
+			var styleText = document.createTextNode('.is-complete' + this._viewId + ' { color: ' + this._completedTextColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._completedColor.length > 0) {
+			var styleText = document.createTextNode('.progress' + this._viewId + ' .progress-step.is-complete:before { background: ' + this._completedColor + '; } ');
+			styleNode.appendChild(styleText);
+			var styleText = document.createTextNode('.progress' + this._viewId + ' .progress-step.is-complete:after { background: ' + this._completedColor + '; } ');
+			styleNode.appendChild(styleText);
+		}
+		if (this._pulseColor && this._pulseColor.length > 0) {
+			var styleText = document.createTextNode("@keyframes pulse" + this._viewId + " {0% {box-shadow: 0 0 0 0 " + this.hexToRgba(this._pulseColor, 40) + " ;} 70% {box-shadow: 0 0 0 10px " + this.hexToRgba(this._pulseColor, 100) + ";} 100% {box-shadow: 0 0 0 0 " + this.hexToRgba(this._pulseColor, 100) + ";} }} ");
+			styleNode.appendChild(styleText);
+		}
+		document.getElementsByTagName('head')[0].appendChild(styleNode);
 	}
 	/**
 	 * * This function convert a hex color to an rgba.
